4e264838a51e6c618d4dc5c1c8634e3f8fecfdcf
Dragos Tatulea <dtatulea@nvidia.com>
2025-03-29 15:08:00 +0100

    net/mlx5e: Create/destroy PCIe Congestion Event object
    Add initial infrastructure to create and destroy the PCIe Congestion
    Event object if the object is supported.
    
    The verb for the object creation function is "set" instead of
    "create" because the function will accommodate the modify operation
    as well in a subsequent patch.
    
    The next patches will hook it up to the event handler and will add
    actual functionality.
    
    Issue: 4318230
    Change-Id: I2f690b7f92849176c8012b55e1bab7c18483c331
    Signed-off-by: Dragos Tatulea <dtatulea@nvidia.com>
    Signed-off-by: Alexei Lazar <alazar@nvidia.com>

---
+++ drivers/net/ethernet/mellanox/mlx5/core/en/pcie_cong_event.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 OR Linux-OpenIB */
+/* Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES. */
+
+#ifndef __MLX5_PCIE_CONG_EVENT_H__
+#define __MLX5_PCIE_CONG_EVENT_H__
+
+bool mlx5e_pcie_cong_event_supported(struct mlx5_core_dev *dev);
+int mlx5e_pcie_cong_event_init(struct mlx5e_priv *priv);
+void mlx5e_pcie_cong_event_cleanup(struct mlx5e_priv *priv);
+
+#endif /* __MLX5_PCIE_CONG_EVENT_H__ */
