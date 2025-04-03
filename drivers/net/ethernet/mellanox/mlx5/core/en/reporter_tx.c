1150b8890c895c0cf0c21dee6e8fa7c36e19eec7
Cosmin Ratiu <cratiu@nvidia.com>
2025-04-03 11:48:45 +0300

    net/mlx5e: Convert mlx5 netdevs to instance locking
    This patch convert mlx5 to use the new netdev instance lock in addition
    to the pre-existing state_lock (and the RTNL).
    
    mlx5e_priv.state_lock was already used throughout mlx5 to protect
    against concurrent state modifications on the same netdev, usually in
    addition to the RTNL. The new netdev instance lock will eventually
    replace it, but for now, it is acquired in addition to the existing
    locks in the order RTNL -> instance lock -> state_lock.
    
    All three netdev types handled by mlx5 are converted to the new style of
    locking, because they share a lot of code related to initializing
    channels and dealing with NAPI, so it's better to convert all three
    rather than introduce different assumptions deep in the call stack
    depending on the type of device.
    
    Because of the nature of the call graphs in mlx5, it wasn't possible to
    incrementally convert parts of the driver to use the new lock, since
    either all call paths into NAPI have to possess the new lock if the
    *_locked variants are used, or none of them can have the lock.
    
    Issue: 3358561
    Signed-off-by: Cosmin Ratiu <cratiu@nvidia.com>
    Change-Id: Ifd7de85534cf3a3d4d257a2d53a8ffd9a79ccb87
    Signed-off-by: Alexei Lazar <alazar@nvidia.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en/reporter_tx.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en/reporter_tx.c
@@ -108,7 +108,9 @@ static int mlx5e_tx_reporter_err_cqe_recover(void *ctx)
 	sq->stats->recover++;
 	clear_bit(MLX5E_SQ_STATE_RECOVERING, &sq->state);
 	rtnl_lock();
+	netdev_lock(dev);
 	mlx5e_activate_txqsq(sq);
+	netdev_unlock(dev);
 	rtnl_unlock();
 
 	if (sq->channel)
@@ -145,9 +147,11 @@ static int mlx5e_tx_reporter_timeout_recover(void *ctx)
 		return err;
 	}
 
+	netdev_lock(priv->netdev);
 	mutex_lock(&priv->state_lock);
 	err = mlx5e_safe_reopen_channels(priv);
 	mutex_unlock(&priv->state_lock);
+	netdev_unlock(priv->netdev);
 	if (!err) {
 		to_ctx->status = 1; /* all channels recovered */
 		return err;
@@ -175,11 +179,12 @@ static int mlx5e_tx_reporter_ptpsq_unhealthy_recover(void *ctx)
 		return 0;
 
 	priv = ptpsq->txqsq.priv;
+	netdev = priv->netdev;
 
 	rtnl_lock();
+	netdev_lock(netdev);
 	mutex_lock(&priv->state_lock);
 	chs = &priv->channels;
-	netdev = priv->netdev;
 
 	carrier_ok = netif_carrier_ok(netdev);
 	netif_carrier_off(netdev);
@@ -196,6 +201,7 @@ static int mlx5e_tx_reporter_ptpsq_unhealthy_recover(void *ctx)
 		netif_carrier_on(netdev);
 
 	mutex_unlock(&priv->state_lock);
+	netdev_unlock(netdev);
 	rtnl_unlock();
 
 	return err;
