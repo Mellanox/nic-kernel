6441d747a868e5acc928602cc95a1aeb73b4fd95
Cosmin Ratiu <cratiu@nvidia.com>
2025-05-26 13:42:59 +0300

    net/mlx5e: Use multiple TX doorbells
    First, allocate more doorbells in mlx5e_create_mdev_resources:
    - one doorbell remains 'global' and will be used by all non-channel
      associated SQs (e.g. ASO, HWS, PTP, ...).
    - allocate additional 'num_doorbells' doorbells. This defaults to
      minimum between 8 and max number of channels.
    
    mlx5e_channel_pick_doorbell() now spreads out channel SQs across
    available doorbells.
    
    Issue: 4379028
    Signed-off-by: Cosmin Ratiu <cratiu@nvidia.com>
    Change-Id: I8cae0778338535c63cabe8aadd5d32bd51d9ae76
    Signed-off-by: Alexei Lazar <alazar@nvidia.com>

--- include/linux/mlx5/driver.h
+++ include/linux/mlx5/driver.h
@@ -655,6 +655,8 @@ struct mlx5e_resources {
 		u32                        pdn;
 		struct mlx5_td             td;
 		u32			   mkey;
+		struct mlx5_sq_bfreg      *bfregs;
+		unsigned int               num_bfregs;
 #define MLX5_MAX_NUM_TC 8
 		u32                        tisn[MLX5_MAX_PORTS][MLX5_MAX_NUM_TC];
 		bool			   tisn_valid;
@@ -795,6 +797,9 @@ struct mlx5_db {
 	dma_addr_t		dma;
 	int			index;
 };
+
+
+#define MLX5_DEFAULT_NUM_DOORBELLS 8
 
 enum {
 	MLX5_COMP_EQ_SIZE = 1024,
