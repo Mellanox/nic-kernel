c11d6b28ec0bee0af1a1213b793da937243dd7a3
Maher Sanalla <msanalla@nvidia.com>
2025-12-16 16:11:56 +0200

    RDMA/mlx5: Add TLP VAR region support and infrastructure
    Add support for TLP (Transaction Layer Packet) VAR regions used by
    software-defined device emulation. TLP VAR provides dedicated response
    gateways for sending TLP responses back to the host in TLP emulation
    scenarios.
    
    Signed-off-by: Maher Sanalla <msanalla@nvidia.com>
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>

--- drivers/infiniband/hw/mlx5/mlx5_ib.h
+++ drivers/infiniband/hw/mlx5/mlx5_ib.h
@@ -162,6 +162,7 @@ enum mlx5_ib_mmap_type {
 	MLX5_IB_MMAP_TYPE_UAR_WC = 3,
 	MLX5_IB_MMAP_TYPE_UAR_NC = 4,
 	MLX5_IB_MMAP_TYPE_MEMIC_OP = 5,
+	MLX5_IB_MMAP_TYPE_TLP_VAR = 6,
 };
 
 struct mlx5_bfreg_info {
@@ -1062,6 +1063,7 @@ struct mlx5_var_region {
 
 struct mlx5_var_table {
 	struct mlx5_var_region var_region;
+	struct mlx5_var_region tlp_var_region;
 };
 
 struct mlx5_port_caps {
