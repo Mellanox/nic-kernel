ca5ab008a83f3ef55907fde674ebfd9ce6a77189
Michael Guralnik <michaelgur@nvidia.com>
2026-05-17 10:12:54 +0300

    RDMA/mlx5: Support new Unordered access flag
    Add mlx5 driver support for IB_ACCESS_UNORDERED, the new uverbs access
    flag that enables relaxing the order of read after write on mkeys.
    
    Extend the current enablement of ATS over DMABUF with RO access to also
    enable for DMABUF with Unordered access.
    
    Mark IB_ACCESS_UNORDERED as non-UMR-modifiable: there is no UMR
    mkey-mask bit for order_read_after_write, so UMR cannot drive it.
    
    Signed-off-by: Michael Guralnik <michaelgur@nvidia.com>
    Reviewed-by: Yishai Hadas <yishaih@nvidia.com>
    Signed-off-by: Edward Srouji <edwards@nvidia.com>

--- drivers/infiniband/hw/mlx5/mr.c
+++ drivers/infiniband/hw/mlx5/mr.c
@@ -71,8 +71,13 @@ static void set_mkc_access_pd_addr_fields(void *mkc, int acc, u64 start_addr,
 	MLX5_SET(mkc, mkc, lw, !!(acc & IB_ACCESS_LOCAL_WRITE));
 	MLX5_SET(mkc, mkc, lr, 1);
 
-	if (acc & IB_ACCESS_RELAXED_ORDERING)
+	if (acc & (IB_ACCESS_RELAXED_ORDERING | IB_ACCESS_UNORDERED))
 		mlx5_core_mkey_set_relaxed_ordering(dev->mdev, mkc);
+
+	if ((acc & IB_ACCESS_UNORDERED) &&
+	    MLX5_CAP_GEN(dev->mdev, mkc_order_read_after_write))
+		MLX5_SET(mkc, mkc, order_read_after_write,
+			 MLX5_MKC_ORDER_READ_AFTER_WRITE_RO);
 
 	MLX5_SET(mkc, mkc, pd, to_mpd(pd)->pdn);
 	MLX5_SET(mkc, mkc, qpn, 0xffffff);
