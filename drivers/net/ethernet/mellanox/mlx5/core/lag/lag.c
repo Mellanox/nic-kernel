369bda24b622ff3063524a1fbe2cf6058956ee55
Or Har-Toov <ohartoov@nvidia.com>
2026-09-02 17:36:28 +0300

    net/mlx5: Lag, limit aggregated TX speed by PCIe bandwidth
    FW initializes max_tx_speed to the minimum of the port speed and the
    PCI bandwidth. Modifying it with a value computed by different rules
    changes the meaning of the field rather than updating it.
    
    Limit each uplink's contribution to the aggregated LAG TX speed by the
    NIC's PCIe bandwidth and reset max_tx_speed to the same value when lag
    is torn down.
    
    Issue: 42252569
    Fixes: 50f1d188c580 ("net/mlx5: Propagate LAG effective max_tx_speed to vports")
    Change-Id: I767058c0d9b0901b7d4fd4d9f317ce0d840fe7bf
    Signed-off-by: Or Har-Toov <ohartoov@nvidia.com>

--- drivers/net/ethernet/mellanox/mlx5/core/lag/lag.c
+++ drivers/net/ethernet/mellanox/mlx5/core/lag/lag.c
@@ -1438,6 +1438,7 @@ static int mlx5_lag_get_devices_oper_speed(struct mlx5_lag *ldev,
 {
 	struct mlx5_core_dev *pf_mdev;
 	struct lag_func *pf;
+	u32 pci_bw;
 	int pf_idx;
 	bool mpesw;
 	u32 speed;
@@ -1472,6 +1473,9 @@ static int mlx5_lag_get_devices_oper_speed(struct mlx5_lag *ldev,
 			return ret;
 		}
 
+		pci_bw = mlx5_pcie_bandwidth(pf_mdev);
+		if (pci_bw)
+			speed = min(speed, pci_bw);
 		*sum_speed += speed;
 	}
 
@@ -1483,6 +1487,7 @@ static int mlx5_lag_get_devices_max_speed(struct mlx5_lag *ldev, u32 *max_speed)
 	struct mlx5_core_dev *pf_mdev;
 	struct lag_func *pf;
 	bool take_max;
+	u32 pci_bw;
 	int pf_idx;
 	u32 speed;
 	int ret;
@@ -1508,6 +1513,9 @@ static int mlx5_lag_get_devices_max_speed(struct mlx5_lag *ldev, u32 *max_speed)
 			return ret;
 		}
 
+		pci_bw = mlx5_pcie_bandwidth(pf_mdev);
+		if (pci_bw)
+			speed = min(speed, pci_bw);
 		*max_speed = take_max ?
 			max(*max_speed, speed) : *max_speed + speed;
 	}
@@ -1608,6 +1616,7 @@ void mlx5_lag_reset_vports_speed(struct mlx5_lag *ldev)
 {
 	struct mlx5_core_dev *mdev;
 	struct lag_func *pf;
+	u32 pci_bw;
 	u32 speed;
 	int pf_idx;
 	int ret;
@@ -1629,6 +1638,9 @@ void mlx5_lag_reset_vports_speed(struct mlx5_lag *ldev)
 			continue;
 		}
 
+		pci_bw = mlx5_pcie_bandwidth(mdev);
+		if (pci_bw)
+			speed = min(speed, pci_bw);
 		speed = speed / MLX5_MAX_TX_SPEED_UNIT;
 		mlx5_lag_modify_device_vports_speed(mdev, speed);
 	}
